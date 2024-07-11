ddeef45373e4371370ba58d4bddfc0d89eaa729d
VladiusVostokus <kovalenkov222@gmail.com>
2024-07-11 13:45:08 +0300

    get files from commands inner folders

--- src/utils/getLocalCommands.js
+++ src/utils/getLocalCommands.js
@@ -6,6 +6,12 @@ const getAllFiles = require("./getAllFiles");
 module.exports = (exceptions) => {
     let localCommands = [];
     const commandCategories = getAllFiles(path.join(__dirname, '..', 'commands'), true);
-    console.log(commandCategories);
+    //console.log(commandCategories);
+
+    for (const commandCategory of commandCategories) {
+        const commandFiles = getAllFiles(commandCategory);
+        console.log(commandFiles);
+    }
+
     return localCommands;
 };
