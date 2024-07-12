017c7d90555e96052b17913a765374791f5c6f90
VladiusVostokus <kovalenkov222@gmail.com>
2024-07-12 13:22:17 +0300

    Get local commands
    function to get commands from commands directory. Commands will be
    created as an objects with priperies and callback as logic of functions.
    This will be used to register and to use these ds bot functions.

--- src/utils/getLocalCommands.js
+++ src/utils/getLocalCommands.js
@@ -3,14 +3,19 @@
 const path = require('path');
 const getAllFiles = require("./getAllFiles");
 
-module.exports = (exceptions) => {
+module.exports = (exceptions = []) => {
     let localCommands = [];
     const commandCategories = getAllFiles(path.join(__dirname, '..', 'commands'), true);
     //console.log(commandCategories);
 
     for (const commandCategory of commandCategories) {
         const commandFiles = getAllFiles(commandCategory);
-        console.log(commandFiles);
+        //console.log(commandFiles);
+        for (const commandFile of commandFiles) {
+            const commandObject = require(commandFile);
+            if (exceptions.includes(commandObject.name)) continue;
+            localCommands.push(commandObject);
+        }
     }
 
     return localCommands;
