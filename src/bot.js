ca5705c1dc4787b3eb40fb825be80a7656810687
VladiusVostokus <kovalenkov222@gmail.com>
2024-07-20 10:57:33 +0300

    move giving role button to foulder structure

--- src/bot.js
+++ src/bot.js
@@ -69,40 +69,6 @@ client.on('ready', async (c) => {
 
 eventHandler(client);
 
-client.on('interactionCreate', async(interaction) => {
-
-  if(!interaction.isButton() &&
-   !interaction.isChatInputCommand()) return;
-
-  if(interaction.isButton()) {
-
-    await interaction.deferReply({ ephemeral: true});
-  
-    const role = interaction.guild.roles.cache.get(
-      interaction.customId
-    );
-
-    if(!role) {
-      interaction.editReply({
-        content: 'Ролі не існує',
-      });
-      return;
-    }
-
-    const hasRole = interaction.member.roles.cache.has(role.id);
-
-    if (hasRole) {
-      await interaction.member.roles.remove(role);
-      await interaction.editReply(`Роль ${role} була взнята`);
-      return;
-    }
-    await interaction.member.roles.add(role);
-    await interaction.editReply(`Роль ${role} була назначена`);
-  }
-  
-});
-
-
 client.login(process.env.TOKEN);
 
 /*
