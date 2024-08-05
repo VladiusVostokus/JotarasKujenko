530a4ab6a5dc3f031714d3dea2ec56d9d0f84504
VladiusVostokus <kovalenkov222@gmail.com>
2024-08-05 20:42:17 +0300

    Change add command for CommandKit

--- src/commands/misc/add.js
+++ src/commands/misc/add.js
@@ -3,33 +3,35 @@
 const {ApplicationCommandOptionType } = require('discord.js');
 
 module.exports = {
-    name: 'add',
-    description: 'Replies sum of 2 numbers',
-    options: [
-        {
-            name: 'first',
-            description: 'The first num',
-            type: ApplicationCommandOptionType.Number,
-            required: true,
-            choices: [
-                {
-                    name: 'one',
-                    value: 1,
-                },
-                {
-                    name: 'two',
-                    value: 2,
-                },
-            ],
-        },
-        {
-            name: 'second',
-            description: 'The second num',
-            type: ApplicationCommandOptionType.Number,
-            required: true,
-        }
-    ],
-    callback: async (client, interaction) => {
+    data: {
+        name: 'add',
+        description: 'Replies sum of 2 numbers',
+        options: [
+            {
+                name: 'first',
+                description: 'The first num',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+                choices: [
+                    {
+                        name: 'one',
+                        value: 1,
+                    },
+                    {
+                        name: 'two',
+                        value: 2,
+                    },
+                ],
+            },
+            {
+                name: 'second',
+                description: 'The second num',
+                type: ApplicationCommandOptionType.Number,
+                required: true,
+            }
+        ],
+    },
+    run: async ({ interaction }) => {
         const num1 = interaction.options.get('first').value;
         const num2 = interaction.options.get('second').value;
         await interaction.reply(`Сума = ${num1 + num2}`);
