e55f2bb6ed3a39dbdbb48c5bca93d706962a97a7
VladiusVostokus <kovalenkov222@gmail.com>
2024-08-05 20:48:37 +0300

    Change ban command for CommandKit

--- src/commands/moderation/ban.js
+++ src/commands/moderation/ban.js
@@ -3,32 +3,33 @@
 const { ApplicationCommandOptionType, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
-    name: 'ban',
-    description: 'ban user', 
+    data: {
+        name: 'ban',
+        description: 'ban user',
+        options:[
+            {
+                name: 'target-user', 
+                description: 'user to ban',
+                required: true,
+                type: ApplicationCommandOptionType.Mentionable
+            }, 
+            {
+                name: 'reason', 
+                description: 'why this user is banned',
+                required: false,
+                type: ApplicationCommandOptionType.String
+            }, 
+        ],
+    }, 
     //devOnly: bool,
     //testOnly: bool,
-    options:[
-        {
-            name: 'target-user', 
-            description: 'user to ban',
-            required: true,
-            type: ApplicationCommandOptionType.Mentionable
-        }, 
-        {
-            name: 'reason', 
-            description: 'why this user is banned',
-            required: false,
-            type: ApplicationCommandOptionType.String
-        }, 
-    ],
 
     //deleted: bool,
-
-    permissionsRequired: [PermissionFlagsBits.Administrator],
-    botPermissions: [PermissionFlagsBits.Administrator],
-    
-    callback: (client, interaction) => {
+    options: {
+        permissionsRequired: [PermissionFlagsBits.Administrator],
+        botPermissions: [PermissionFlagsBits.Administrator],
+    },
+    run: ({ interaction }) => {
         interaction.reply(`YOU ARE BANNED`);
     },
-    
 };
