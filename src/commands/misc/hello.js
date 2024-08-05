6b53c96aefc17ba46dfd837161d9e1f9ad211c61
VladiusVostokus <kovalenkov222@gmail.com>
2024-08-05 20:46:48 +0300

    Change hello command for CommandKit

--- src/commands/misc/hello.js
+++ src/commands/misc/hello.js
@@ -1,13 +1,15 @@
 'use strict';
 
 module.exports = {
-    name: 'hello',
-    description: 'Replies with hello', 
+    data: {
+        name: 'hello',
+        description: 'Replies with hello', 
+    },
     //devOnly: bool,
     //testOnly: bool,
     //options:[{array: of, all: aptions}, {some: other_options}],
     //deleted: true,
-    callback: async (client, interaction) => {
+    run: async ({ interaction }) => {
         await interaction.reply('hello!');
     },
 };
